3a3539a7ac54dc02082bb56caefa17c4d108461d
oddlyspaced <oddlyspaced1@gmail.com>
2020-11-07 20:15:54 +0530

    FeedPost: Setup modal for post in feed

---
+++ src/main/kotlin/modal/FeedPost.kt
@@ -0,0 +1,13 @@
+package modal
+
+data class FeedPost(
+    val title: String,
+    val link: String,
+    val thumbnail: String,
+    val author: String,
+    val authorThumbnail: String,
+    val authorLink: String,
+    val badge: String,
+    val likes: Int,
+    val comments: Int
+)
